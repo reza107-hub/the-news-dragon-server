3aef385740f5b99c415902f73273d7bb67b7d81f
reza107-hub <rashidaminur104@gmail.com>
2023-04-27 19:51:30 +0600

    news added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const catagories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -11,7 +12,6 @@ app.use(cors())
 app.get('/catagories', (req, res) => {
     res.send(catagories)
 })
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/news', (req, res) => {
+    res.send(news)
 })
