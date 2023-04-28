51dc609941449ccbb0ae5fce49c768dce77e6ba9
reza107-hub <rashidaminur104@gmail.com>
2023-04-28 19:38:10 +0600

    news added

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ app.get('/news', (req, res) => {
     res.send(news)
 })
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n=> n._id === id)
+    res.send(selectedNews)
+})
+app.get('/catagories/:id', (req, res) => {
+    const id = req.params.id
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const selectedNews = news.filter(n=> n.category_id === id)
+    res.send(selectedNews)
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
